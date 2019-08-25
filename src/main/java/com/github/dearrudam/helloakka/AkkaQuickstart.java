6f8a72e95a73f0b807b3b89b5bae67265eb7507b
Maximillian Arruda <dearrudam@gmail.com>
2019-08-25 00:44:51 +0000

    First commit

---
+++ src/main/java/com/github/dearrudam/helloakka/AkkaQuickstart.java
@@ -0,0 +1,12 @@
+package com.github.dearrudam.helloakka;
+
+/**
+ * AkkaQuickstart
+ */
+public class AkkaQuickstart {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
